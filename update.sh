507c3fda47a14ca62242f0c5728a229a1b0bc770
Michael Gooch <mgooch@kotoro.com>
2013-10-29 16:41:07 -0400

    some scripts

---
+++ update.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+git fetch --all
+git reset --hard origin/master
+function EXPERM{
+	DIR=$1;
+	shift;
+	for EXT in "$@"
+	do 
+	find ./$DIR -type f -path *.$EXT -exec chmod -f +x {} \;
+	done	
+}
+function EXPERMALL{
+	DIR=$1;
+	shift;
+	find ./$DIR -type f -exec chmod -f +x {} \;	
+}
+EXPERM cgi-bin pl pm
