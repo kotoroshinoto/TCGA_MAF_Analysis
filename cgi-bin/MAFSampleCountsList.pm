fe95b7d9e0e8ca17a499fcd97d0a3394b940ddf1
mgooch <mgooch@kotoro.com>
2014-03-20 01:25:15 -0400

    Might change file's name, could probably use this object for other types

--- cgi-bin/MAFSampleCountsList.pm
+++ cgi-bin/MAFSampleCountsList.pm
@@ -1,14 +1,43 @@
-package MAFSampleCountsList
-sub new{
-	
-}
 
-sub readFile{
+package MAFSampleCountsList;
+	use strict;
+	use warnings;
+	use Carp qw(cluck);
+	use Cwd;
+	use Cwd 'abs_path';
+	use File::Basename;
+	use Getopt::Long qw(:config no_ignore_case bundling);
+	use List::MoreUtils qw(uniq);
+	use FileHandle;
+	use Scalar::Util;
 	
-}
-
-sub _clear{
+#	use vars qw/$dirname/;
+#	BEGIN {
+#		$dirname = dirname(__FILE__);
+#	}
+#	use lib $dirname;
+#	use MAFcounters;
 	
-}
+	sub new{
+		my $class =shift;
+		my $self={
+			counts=>{},
+			_keys=>()
+		};
+		bless $self,$class;
+		return $self;
+	}
+	
+	sub getCount{
+		
+	}
+	
+	sub readFile{
+		
+	}
+	
+	sub _clear{
+		
+	}
 
 1;
