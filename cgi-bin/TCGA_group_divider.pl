3ca0570d5e2c8e2c2bd05af66df6d8f2076ce9e0
Kotoro Shinoto <mgooch@kotoro.com>
2014-03-24 13:00:21 -0400

    working on converting copied script to function for new purpose

--- cgi-bin/TCGA_group_divider.pl
+++ cgi-bin/TCGA_group_divider.pl
@@ -10,26 +10,29 @@ use FileHandle;
 use Scalar::Util;
 use vars qw/$dirname/;
 BEGIN {
+#	print "loading perl library: ".__FILE__,"\n";
 	$dirname = dirname(__FILE__);
+#	print "loading perl library: $dirname\n";
 }
 use lib $dirname;
 use MAFentry;
 use MAFfile;
 use MAFcounters;
-
+use MAFSampleCountsList;
 
 #define object types used in this process:
 
 package main;
 my $help=0;#indicates usage should be shown and nothing should be done
-my ($illuminaFile,$solidFile,$opts);
-our ($countGene,$countPatient,$countMutType)= (0) x 3;
+my ($MAF_File,$CountFile,$opts);
+#our ($countGene,$countPatient,$countMutType)= (0) x 3;
 
-$opts = GetOptions ("Illumina|f=s" => \$illuminaFile,	# path to illumina data
-						"Solid|F=s"   => \$solidFile,	# path to solid data
-						"countGene|G" => \$countGene, #list of steps to assume were already run, assume order as well (acts like pipeline)
-						"countPatient|P" => \$countPatient,
-						"countMutType|M" => \$countMutType,
+$opts = GetOptions (
+						"MAF_file|m=s" => \$MAF_File,	# path to illumina data
+						"Count_File|c=s"   => \$CountFile,	# path to solid data
+#						"countGene|G" => \$countGene, #list of steps to assume were already run, assume order as well (acts like pipeline)
+#						"countPatient|P" => \$countPatient,
+#						"countMutType|M" => \$countMutType,
 						"help|h" =>\$help);
 #print "$joinSOLO, $joinSTEPS, $splitCROSS\n";
 sub ShowUsage {
@@ -48,44 +51,45 @@ if($help){
 	ShowUsage();
 	exit (0);#being asked to show help isn't an error
 }
-if(((not defined($illuminaFile)) and (not defined($solidFile))) or (length($illuminaFile) == 0 and length($solidFile) == 0)){
+if(((not defined($MAF_File)) and (not defined($CountFile))) or (length($MAF_File) == 0 and length($CountFile) == 0)){
 	ShowUsage("must provide at least one of the MAF files");
 }
-if (not($countGene or $countPatient or $countMutType)){
-	ShowUsage("must choose at least one of the count options");
-}
 #create count objects and store as references
-sub CountMafFile{
-	my @counters;
-	if($countGene){
-		my $tmp=GeneMutCounter->new();
-		$tmp->{name}="Genes";
-		push(@counters,$tmp);
-	}
-	if($countPatient){
-		my $tmp=SampMutCounter->new();
-		$tmp->{name}="Samples";
-		push(@counters,$tmp);
-	}
-	if($countMutType){
-		my $tmp=MutTypeCounter->new();
-		$tmp->{name}="MutationTypes";
-		push(@counters,$tmp);
-	}
-	my $maf=MAFfile->open($_[0]);
-	#count line-by-line
-	my $entry;	
-	while ($maf->hasMoreEntries()){
-		$entry=$maf->getNextEntry();
-		#skip first line (its the header)
-		if(isCountable($entry)){
-			foreach my $counter(@counters){
-				$counter->count($entry);
-			}
-		}
-	}
-	$maf->close();
-	return @counters;
+sub SplitMafFile{
+#	my @counters;
+#	if($countGene){
+#		my $tmp=GeneMutCounter->new();
+#		$tmp->{name}="Genes";
+#		push(@counters,$tmp);
+#	}
+#	if($countPatient){
+#		my $tmp=SampMutCounter->new();
+#		$tmp->{name}="Samples";
+#		push(@counters,$tmp);
+#	}
+#	if($countMutType){
+#		my $tmp=MutTypeCounter->new();
+#		$tmp->{name}="MutationTypes";
+#		push(@counters,$tmp);
+#	}
+#	my $maf=MAFfile->open($_[0]);
+#	#count line-by-line
+#	my $entry;	
+#	while ($maf->hasMoreEntries()){
+#		$entry=$maf->getNextEntry();
+#		#skip first line (its the header)
+#		if(isCountable($entry)){
+#			foreach my $counter(@counters){
+#				$counter->count($entry);
+#			}
+#		}
+#	}
+#	$maf->close();
+#	return @counters;
+}
+
+sub getGroupIndex{
+	
 }
 
 sub prepareGroups{
@@ -107,30 +111,30 @@ sub prepareGroups{
 		}
 		$lastval=$boundaries[$i];
 	}
+	return $countlist;
 }
 
-sub isCountable{
-	my $maf=$_[0];
-	#TODO logic on whether to keep entry in count
-	return 1;
+sub main{
+	my @IlluminaList;
+	if(defined($MAF_File) and length($MAF_File) > 0){
+		@IlluminaList=@{prepareGroups($MAF_File)};
+	} else {
+		
+	}
+	
+	my @SOLiDList;
+	if(defined($CountFile) and length($CountFile) > 0){
+		@SOLiDList=@{prepareGroups($CountFile)};
+	} else {
+		
+	}
+	
+	foreach my $Illuminalist(@IlluminaList){
+	#	print ($Illuminacounter->toString());
+	}
+	
+	foreach my $SOLiDCounter(@SOLiDList){
+	#	print ($SOLiDCounter->toString());
+	}
 }
-
-my @IlluminaCounters;
-if(defined($illuminaFile) and length($illuminaFile) > 0){
-	@IlluminaCounters=CountMafFile($illuminaFile);
-}
-
-my @SOLiDCounters;
-if(defined($solidFile) and length($solidFile) > 0){
-	@SOLiDCounters=CountMafFile($solidFile);
-}
-
-foreach my $Illuminacounter(@IlluminaCounters){
-#	print ($Illuminacounter->toString());
-	$Illuminacounter->writeFile("Illumina","");
-}
-
-foreach my $SOLiDCounter(@SOLiDCounters){
-#	print ($SOLiDCounter->toString());
-	$SOLiDCounter->writeFile("SOLiD");
-}
+main();
