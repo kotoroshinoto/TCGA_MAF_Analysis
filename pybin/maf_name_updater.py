53a23e257a271e07e0b5a34e5a446fc0bb26b543
Kotoro Shinoto <mgooch@kotoro.com>
2015-09-09 00:12:21 -0400

    some fixes

--- pybin/maf_name_updater.py
+++ pybin/maf_name_updater.py
@@ -14,11 +14,22 @@ toCol = int(args.column) - 1
 mafnames_fh = args.mafnames
 MAF_names = list()
 
+#pull MAF names from file
 for line in mafnames_fh:
 	line = line.rstrip()
 	MAF_names.append(line)
 mafnames_fh.close()
 
+#build map of names from symbolcheck file
+symbolcheck_map = dict()
+if args.symbolcheck is not None:
+	for line in args.symbolcheck:
+		line = line.rstrip()
+		split_line = line.split("\t")
+		if len(split_line) > 3:
+			symbolcheck_map[split_line[0]] = split_line[2]
+
+#build list of lines from names file
 names_fh = args.names
 name_lines = list()
 for line in names_fh:
@@ -26,7 +37,8 @@ for line in names_fh:
 	if len(line) > 0:
 		name_lines.append(str(line.rstrip()))
 
-Easy_List = list()
+
+#build list of names from size file if one was given
 Size_File_Name_List = list()
 if args.size is not None:
 	for line in args.size:
@@ -37,6 +49,8 @@ if args.size is not None:
 			Size_File_Name_List.append(name)
 			#print("added name: %s" % name)
 
+#build list of names from names file for quick check
+Easy_List = list()
 for line in name_lines:
 	line = line.rstrip()
 	if len(line) > 0:
@@ -44,18 +58,24 @@ for line in name_lines:
 		Easy_List.append(split_line[toCol])
 
 Name_Map = dict()
-
+for name in MAF_names:
+	if name not in Name_Map:
+		if name in Size_File_Name_List:
+			print("original name OK for: %s" % name, file=sys.stderr)
+			Name_Map[name] = name
 for name in MAF_names:
 	if name not in Name_Map:
 		if name in Easy_List:
 			print("original name OK for: %s" % name, file=sys.stderr)
 			Name_Map[name] = name
 for name in MAF_names:
-	if name not in Name_Map:
-		if name in Size_File_Name_List:
-			print("original name OK for: %s" % name, file=sys.stderr)
-			Name_Map[name] = name
-
+	if name in Name_Map:
+		if name in symbolcheck_map:
+			if Name_Map[name] != symbolcheck_map[name]:
+				print("symbolcheck name conflicts with existing choice for: %s | %s; %s" % (name, Name_Map[name], symbolcheck_map[name]), file=sys.stderr)
+	else:
+		if name in symbolcheck_map:
+			Name_Map[name] = symbolcheck_map[name]
 
 for name in MAF_names:
 	match_found = False
@@ -64,14 +84,15 @@ for name in MAF_names:
 	else:
 		sys.stderr.write("searching for a match for %s; " % name)
 		for line in name_lines:
-			regex_matcher_str = "[\s]"+name+"[,\s]"
+			regex_matcher_str = "[\s]" + name + "[,\s]"
 			matchobj = re.search(regex_matcher_str, line)
 			if matchobj:
 				match_found = True
 				print("line: %s" % line, file=sys.stderr)
 				split_line = line.split("\t")
 				to_name = split_line[toCol]
-				print("%s\t%s" % (name, to_name))
+				Name_Map[name] = to_name
+				# print("%s\t%s" % (name, to_name))
 	if not match_found:
 		print("no match was found for: %s" % name, file=sys.stderr)
 for name in Name_Map:
