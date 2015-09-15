8c0eb25f72b295247e29f46ce90a110c57035abb
mgooch <goochmi@gmail.com>
2015-09-15 18:43:02 -0400

    fairly major oopsie. Checking NEW names in 2 column file against length file is now possible

--- pybin/Annotation/MAF/Name_Mappers/check_against_lengths_file.py
+++ pybin/Annotation/MAF/Name_Mappers/check_against_lengths_file.py
@@ -3,14 +3,17 @@ __author__ = 'mgooch'
 import argparse
 import re
 import sys
+import os
 parser = argparse.ArgumentParser(description="Check MAF names against names in Gene Length files")
 parser.add_argument('--mafnames', type=argparse.FileType('r'), required=True, help="file of names to be translated")
 parser.add_argument('--checkcolumn', type=int, help="check this column instead of oldcolumn")
 parser.add_argument('--oldcolumn', type=int, default=0, help="this column is the original name")
-parser.add_argument('--genelength', type=argparse.FileType('r'), required=True, help="file of gene lengths that has names")
+parser.add_argument('--genelength', type=argparse.FileType('r'), required=True, help="file of gene lengths that has names", nargs='+')
 parser.add_argument('--matched', type=argparse.FileType('w'), required=True, help="file to write matched names")
 parser.add_argument('--unmatched', type=argparse.FileType('w'), required=True, help="file to write unmatched names")
-parser.add_argument('--keep', default=False, action='store_true', help="keep newcolumn (or oldcolumn if not set) label in 2nd column if unmatched")
+parser.add_argument('--keep', default=False, action='store_true', help="put checked label in 2nd column even if unmatched")
+parser.add_argument('--logOK', default=os.devnull, type=argparse.FileType('w'), help="log OK output to this file")
+parser.add_argument('--logBAD', default=os.devnull, type=argparse.FileType('w'), help="log BAD output to this file")
 
 args = parser.parse_args()
 
@@ -19,8 +22,7 @@ MAF_names = dict()
 
 oldcolumn = args.oldcolumn
 checkcolumn = oldcolumn
-
-if not hasattr(args, 'newcolumn'):
+if args.checkcolumn is not None:
 	checkcolumn = args.checkcolumn
 
 #pull MAF names from file
@@ -38,27 +40,29 @@ args.mafnames.close()
 
 #build list of names from size file if one was given
 Length_File_Name_List = dict()
-
-for line in args.genelength:
-	if len(line) > 0:
-		split_line = line.split("\t")
-		symbol = split_line[0].rstrip()
-		if len(symbol) > 0:
-			Length_File_Name_List[symbol.upper()] = symbol
-args.genelength.close()
+for lengthfile in args.genelength:
+	print("reading file: %s" % lengthfile.name)
+	for line in lengthfile:
+		if len(line) > 0:
+			split_line = line.split("\t")
+			symbol = split_line[0].rstrip()
+			if len(symbol) > 0:
+				if symbol.upper() not in Length_File_Name_List:
+					Length_File_Name_List[symbol.upper()] = symbol
+	lengthfile.close()
 
 for check_symbol in MAF_names:
 	if check_symbol in Length_File_Name_List:
 		if oldcolumn != checkcolumn:
-			print("checked name OK for: %s -> %s" % (MAF_original_names[check_symbol], MAF_names[check_symbol]), file=sys.stderr)
+			print("checked name OK for: %s -> %s" % (MAF_original_names[check_symbol], MAF_names[check_symbol]), file=args.logOK)
 		else:
-			print("checked name OK for: %s" % (MAF_names[check_symbol]), file=sys.stderr)
+			print("checked name OK for: %s" % (MAF_names[check_symbol]), file=args.logOK)
 		print("%s\t%s" % (MAF_original_names[check_symbol], MAF_names[check_symbol]), file=args.matched)
 	else:
 		if oldcolumn != checkcolumn:
-			print("checked name BAD for: %s - > %s" % (MAF_original_names[check_symbol], MAF_names[check_symbol]), file=sys.stderr)
+			print("checked name BAD for: %s - > %s" % (MAF_original_names[check_symbol], MAF_names[check_symbol]), file=args.logBAD)
 		else:
-			print("checked name BAD for: %s" % (MAF_names[check_symbol]), file=sys.stderr)
+			print("checked name BAD for: %s" % (MAF_names[check_symbol]), file=args.logBAD)
 		if args.keep:
 			print("%s\t%s" % (MAF_original_names[check_symbol], MAF_names[check_symbol]), file=args.unmatched)
 		else:
