0cdc8608f2a02d229d5423ac840213be34c65df5
mgooch <goochmi@gmail.com>
2015-09-15 16:53:28 -0400

    fairly major oopsie. Checking NEW names in 2 column file against length file is now possible

--- pybin/Annotation/MAF/Name_Mappers/check_against_lengths_file.py
+++ pybin/Annotation/MAF/Name_Mappers/check_against_lengths_file.py
@@ -5,21 +5,35 @@ import re
 import sys
 parser = argparse.ArgumentParser(description="Check MAF names against names in Gene Length files")
 parser.add_argument('--mafnames', type=argparse.FileType('r'), required=True, help="file of names to be translated")
+parser.add_argument('--checkcolumn', type=int, help="check this column instead of oldcolumn")
+parser.add_argument('--oldcolumn', type=int, default=0, help="this column is the original name")
 parser.add_argument('--genelength', type=argparse.FileType('r'), required=True, help="file of gene lengths that has names")
 parser.add_argument('--matched', type=argparse.FileType('w'), required=True, help="file to write matched names")
 parser.add_argument('--unmatched', type=argparse.FileType('w'), required=True, help="file to write unmatched names")
+parser.add_argument('--keep', default=False, action='store_true', help="keep newcolumn (or oldcolumn if not set) label in 2nd column if unmatched")
 
 args = parser.parse_args()
 
+MAF_original_names = dict()
 MAF_names = dict()
+
+oldcolumn = args.oldcolumn
+checkcolumn = oldcolumn
+
+if not hasattr(args, 'newcolumn'):
+	checkcolumn = args.checkcolumn
 
 #pull MAF names from file
 for line in args.mafnames:
 	if len(line) > 0:
 		split_line = line.split("\t")
-		symbol = split_line[0].rstrip()
-		if len(symbol) > 0:
-			MAF_names[symbol.upper()] = symbol
+		old_symbol = split_line[oldcolumn].rstrip()
+		check_symbol = split_line[checkcolumn].rstrip()
+		if len(check_symbol) > 0:
+			MAF_names[check_symbol.upper()] = check_symbol
+			if len(old_symbol) == 0:
+				print("[WARNING] missing original symbol for checked symbol: %s" % check_symbol)
+			MAF_original_names[check_symbol.upper()] = old_symbol
 args.mafnames.close()
 
 #build list of names from size file if one was given
@@ -28,17 +42,26 @@ Length_File_Name_List = dict()
 for line in args.genelength:
 	if len(line) > 0:
 		split_line = line.split("\t")
-		name = split_line[0].rstrip()
-		if len(name) > 0:
-			Length_File_Name_List[name.upper()] = name
+		symbol = split_line[0].rstrip()
+		if len(symbol) > 0:
+			Length_File_Name_List[symbol.upper()] = symbol
 args.genelength.close()
 
-for name in MAF_names:
-	if name in Length_File_Name_List:
-		print("original name OK for: %s" % MAF_names[name], file=sys.stderr)
-		print("%s\t%s" % (MAF_names[name], MAF_names[name]), file=args.matched)
+for check_symbol in MAF_names:
+	if check_symbol in Length_File_Name_List:
+		if oldcolumn != checkcolumn:
+			print("checked name OK for: %s -> %s" % (MAF_original_names[check_symbol], MAF_names[check_symbol]), file=sys.stderr)
+		else:
+			print("checked name OK for: %s" % (MAF_names[check_symbol]), file=sys.stderr)
+		print("%s\t%s" % (MAF_original_names[check_symbol], MAF_names[check_symbol]), file=args.matched)
 	else:
-		print("original name BAD for: %s" % MAF_names[name], file=sys.stderr)
-		print("%s\t" % MAF_names[name], file=args.unmatched)
+		if oldcolumn != checkcolumn:
+			print("checked name BAD for: %s - > %s" % (MAF_original_names[check_symbol], MAF_names[check_symbol]), file=sys.stderr)
+		else:
+			print("checked name BAD for: %s" % (MAF_names[check_symbol]), file=sys.stderr)
+		if args.keep:
+			print("%s\t%s" % (MAF_original_names[check_symbol], MAF_names[check_symbol]), file=args.unmatched)
+		else:
+			print("%s\t" % MAF_original_names[check_symbol], file=args.unmatched)
 args.matched.close()
 args.unmatched.close()
--- pybin/Annotation/MAF/Name_Mappers/parse_symbolcheck_output.py
+++ pybin/Annotation/MAF/Name_Mappers/parse_symbolcheck_output.py
@@ -14,24 +14,42 @@ args = parser.parse_args()
 symbolcheck_approved = dict()
 symbolcheck_unmatched = list()
 symbolcheck_updated = dict()
+symbolcheck_synonyms = dict()
+symbolcheck_withdrawn = list()
 
+first_line = True
 if args.symbolcheck is not None:
 	for line in args.symbolcheck:
+		if first_line:
+			first_line = False
+			continue
 		split_line = line.split("\t")
 		category = split_line[1].rstrip()
 		old_symbol = split_line[0].rstrip()
 		if category == "Unmatched":
 			symbolcheck_unmatched.append(old_symbol)
 			continue
+		elif category == "Withdrawn":
+			symbolcheck_withdrawn.append(old_symbol)
+			continue
 		new_symbol = split_line[2].rstrip()
 		if category == "Approved symbol":
 			symbolcheck_approved[old_symbol] = new_symbol
 		elif category == "Previous symbol":
 			symbolcheck_updated[old_symbol] = new_symbol
+		elif category == "Synonyms":
+			symbolcheck_synonyms[old_symbol] = new_symbol
+		else:
+			print("unrecognized category: %s" % category, file=sys.stderr)
 
 for symbol in symbolcheck_approved:
 	print("%s\t%s" % (symbol, symbolcheck_approved[symbol]), file=args.outApproved)
 for symbol in symbolcheck_unmatched:
-	print("%s\t%s" % (symbol, symbolcheck_unmatched[symbol]), file=args.outUnmatched)
+	print("%s\t" % symbol, file=args.outUnmatched)
 for symbol in symbolcheck_updated:
 	print("%s\t%s" % (symbol, symbolcheck_updated[symbol]), file=args.outCorrected)
+for symbol in symbolcheck_synonyms:
+	if symbol not in symbolcheck_approved and symbol not in symbolcheck_updated:
+		print("%s\t%s" % (symbol, symbolcheck_synonyms[symbol]), file=args.outCorrected)
+for symbol in symbolcheck_withdrawn:
+	print("[WARNING] symbol withdrawn: %s ", file=sys.stderr)
