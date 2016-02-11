e92a2e9ebef3fb3305ce198ea48ce861fac790da
mgooch <goochmi@gmail.com>
2016-02-11 15:39:21 -0500

    started switching more scripts to use click instead of argparse

--- pybin/check_against_lengths_file.py
+++ pybin/check_against_lengths_file.py
@@ -1,72 +1,82 @@
 #!/usr/bin/env python3
 __author__ = 'mgooch'
-import argparse
+import click
 import re
 import sys
 import os
 
-parser = argparse.ArgumentParser(description="Check Util names against names in Gene Length files")
-parser.add_argument('--mafnames', type=argparse.FileType('r'), required=True, help="file of names to be translated")
-parser.add_argument('--checkcolumn', type=int, help="check this column instead of oldcolumn")
-parser.add_argument('--oldcolumn', type=int, default=0, help="this column is the original name")
-parser.add_argument('--genelength', type=argparse.FileType('r'), required=True, help="file of gene lengths that has names", nargs='+')
-parser.add_argument('--matched', type=argparse.FileType('w'), required=True, help="file to write matched names")
-parser.add_argument('--unmatched', type=argparse.FileType('w'), required=True, help="file to write unmatched names")
-parser.add_argument('--keep', default=False, action='store_true', help="put checked label in 2nd column even if unmatched")
-parser.add_argument('--logOK', default=os.devnull, type=argparse.FileType('w'), help="log OK output to this file")
-parser.add_argument('--logBAD', default=os.devnull, type=argparse.FileType('w'), help="log BAD output to this file")
+# parser = argparse.ArgumentParser(description="Check Util names against names in Gene Length files")
+# parser.add_argument('--mafnames', type=argparse.FileType('r'), required=True, help="file of names to be translated")
+# parser.add_argument('--checkcolumn', type=int, help="check this column instead of oldcolumn")
+# parser.add_argument('--oldcolumn', type=int, default=0, help="this column is the original name")
+# parser.add_argument('--genelength', type=argparse.FileType('r'), required=True, help="file of gene lengths that has names", nargs='+')
+# parser.add_argument('--matched', type=argparse.FileType('w'), required=True, help="file to write matched names")
+# parser.add_argument('--unmatched', type=argparse.FileType('w'), required=True, help="file to write unmatched names")
+# parser.add_argument('--keep', default=False, action='store_true', help="put checked label in 2nd column even if unmatched")
+# parser.add_argument('--logOK', default=os.devnull, type=argparse.FileType('w'), help="log OK output to this file")
+# parser.add_argument('--logBAD', default=os.devnull, type=argparse.FileType('w'), help="log BAD output to this file")
 
-args = parser.parse_args()
 
-MAF_original_names = dict()
-MAF_names = dict()
+@click.command()
+@click.option('--mafnames', type=click.File('r'), required=True, help="file of names to be translated")
+@click.option('--checkcolumn', type=int, default=None, help="check this column instead of oldcolumn")
+@click.option('--oldcolumn', type=int, default=0, help="this column is the original name")
+@click.option('--genelength', type=click.File('r'), required=True, help="file of gene lengths that has names", nargs='+')
+@click.option('--matched', type=click.File('w'), required=True, help="file to write matched names")
+@click.option('--unmatched', type=click.File('w'), required=True, help="file to write unmatched names")
+@click.option('--keep', default=False, is_flag=True, help="put checked label in 2nd column even if unmatched")
+@click.option('--logOK', default=os.devnull, type=click.File('w'), help="log OK output to this file")
+@click.option('--logBAD', default=os.devnull, type=click.File('w'), help="log BAD output to this file")
+def cli(mafnames, checkcolumn, oldcolumn, genelength, matched, unmatched, keep, logOK, logBAD):
+	MAF_original_names = dict()
+	MAF_names = dict()
+	if checkcolumn is None:
+		checkcolumn = oldcolumn
 
-oldcolumn = args.oldcolumn
-checkcolumn = oldcolumn
-if args.checkcolumn is not None:
-	checkcolumn = args.checkcolumn
-
-#pull Util names from file
-for line in args.mafnames:
-	if len(line) > 0:
-		split_line = line.split("\t")
-		old_symbol = split_line[oldcolumn].rstrip()
-		check_symbol = split_line[checkcolumn].rstrip()
-		if len(check_symbol) > 0:
-			MAF_names[check_symbol.upper()] = check_symbol
-			if len(old_symbol) == 0:
-				print("[WARNING] missing original symbol for checked symbol: %s" % check_symbol)
-			MAF_original_names[check_symbol.upper()] = old_symbol
-args.mafnames.close()
-
-#build list of names from size file if one was given
-Length_File_Name_List = dict()
-for lengthfile in args.genelength:
-	print("reading file: %s" % lengthfile.name)
-	for line in lengthfile:
+	#pull Util names from file
+	for line in mafnames:
 		if len(line) > 0:
 			split_line = line.split("\t")
-			symbol = split_line[0].rstrip()
-			if len(symbol) > 0:
-				if symbol.upper() not in Length_File_Name_List:
-					Length_File_Name_List[symbol.upper()] = symbol
-	lengthfile.close()
+			old_symbol = split_line[oldcolumn].rstrip()
+			check_symbol = split_line[checkcolumn].rstrip()
+			if len(check_symbol) > 0:
+				MAF_names[check_symbol.upper()] = check_symbol
+				if len(old_symbol) == 0:
+					print("[WARNING] missing original symbol for checked symbol: %s" % check_symbol)
+				MAF_original_names[check_symbol.upper()] = old_symbol
+	mafnames.close()
 
-for check_symbol in MAF_names:
-	if check_symbol in Length_File_Name_List:
-		if oldcolumn != checkcolumn:
-			print("checked name OK for: %s -> %s" % (MAF_original_names[check_symbol], MAF_names[check_symbol]), file=args.logOK)
+	#build list of names from size file if one was given
+	Length_File_Name_List = dict()
+	for lengthfile in genelength:
+		print("reading file: %s" % lengthfile.name)
+		for line in lengthfile:
+			if len(line) > 0:
+				split_line = line.split("\t")
+				symbol = split_line[0].rstrip()
+				if len(symbol) > 0:
+					if symbol.upper() not in Length_File_Name_List:
+						Length_File_Name_List[symbol.upper()] = symbol
+		lengthfile.close()
+
+	for check_symbol in MAF_names:
+		if check_symbol in Length_File_Name_List:
+			if oldcolumn != checkcolumn:
+				print("checked name OK for: %s -> %s" % (MAF_original_names[check_symbol], MAF_names[check_symbol]), file=logOK)
+			else:
+				print("checked name OK for: %s" % (MAF_names[check_symbol]), file=logOK)
+			print("%s\t%s" % (MAF_original_names[check_symbol], MAF_names[check_symbol]), file=matched)
 		else:
-			print("checked name OK for: %s" % (MAF_names[check_symbol]), file=args.logOK)
-		print("%s\t%s" % (MAF_original_names[check_symbol], MAF_names[check_symbol]), file=args.matched)
-	else:
-		if oldcolumn != checkcolumn:
-			print("checked name BAD for: %s - > %s" % (MAF_original_names[check_symbol], MAF_names[check_symbol]), file=args.logBAD)
-		else:
-			print("checked name BAD for: %s" % (MAF_names[check_symbol]), file=args.logBAD)
-		if args.keep:
-			print("%s\t%s" % (MAF_original_names[check_symbol], MAF_names[check_symbol]), file=args.unmatched)
-		else:
-			print("%s\t" % MAF_original_names[check_symbol], file=args.unmatched)
-args.matched.close()
-args.unmatched.close()
+			if oldcolumn != checkcolumn:
+				print("checked name BAD for: %s - > %s" % (MAF_original_names[check_symbol], MAF_names[check_symbol]), file=logBAD)
+			else:
+				print("checked name BAD for: %s" % (MAF_names[check_symbol]), file=logBAD)
+			if keep:
+				print("%s\t%s" % (MAF_original_names[check_symbol], MAF_names[check_symbol]), file=unmatched)
+			else:
+				print("%s\t" % MAF_original_names[check_symbol], file=unmatched)
+	matched.close()
+	unmatched.close()
+
+if __name__ == "__main__":
+	cli()
--- pybin/update_names_entrez.py
+++ pybin/update_names_entrez.py
@@ -1,71 +1,84 @@
 #!/usr/bin/env python3
 __author__ = 'mgooch'
-import argparse
 import re
 import sys
-parser = argparse.ArgumentParser(description="Compute exonic sizes of genes and relate them to HUGO IDs")
-parser.add_argument('--mafnames', type=argparse.FileType('r'), required=True, help="file of names to be translated")
-parser.add_argument('--name_to_entrez', type=argparse.FileType('r'), required=True, help="output from MAF_collect_unique_entrez_ids.py")
-parser.add_argument('--entrez', type=argparse.FileType('r'), required=True, help="file containing symbols and entrez ids")
-parser.add_argument('--col-entrez', type=int, required=True, help="column that has entrez ids")
-parser.add_argument('--col-symbol', type=int, required=True, help="column that has symbols")
+import click
 
-parser.add_argument('--outCorrected', type=argparse.FileType('w'), required=True, help="file to use for output")
-parser.add_argument('--outUnmatched', type=argparse.FileType('w'), required=True, help="file to use for output")
+# parser = argparse.ArgumentParser(description="Compute exonic sizes of genes and relate them to HUGO IDs")
+# parser.add_argument('--mafnames', type=argparse.FileType('r'), required=True, help="file of names to be translated")
+# parser.add_argument('--name_to_entrez', type=argparse.FileType('r'), required=True, help="output from MAF_collect_unique_entrez_ids.py")
+# parser.add_argument('--entrez', type=argparse.FileType('r'), required=True, help="file containing symbols and entrez ids")
+# parser.add_argument('--col-entrez', type=int, required=True, help="column that has entrez ids")
+# parser.add_argument('--col-symbol', type=int, required=True, help="column that has symbols")
+#
+# parser.add_argument('--outCorrected', type=argparse.FileType('w'), required=True, help="file to use for output")
+# parser.add_argument('--outUnmatched', type=argparse.FileType('w'), required=True, help="file to use for output")
+#
+# args = parser.parse_args()
 
-args = parser.parse_args()
 
+@click.command()
+@click.option('--mafnames', type=click.File('r'), required=True, help="file of names to be translated")
+@click.option('--name_to_entrez', type=click.File('r'), required=True, help="output from MAF_collect_unique_entrez_ids.py")
+@click.option('--entrez', type=click.File('r'), required=True, help="file containing symbols and entrez ids")
+@click.option('--col-entrez', type=int, required=True, help="column that has entrez ids")
+@click.option('--col-symbol', type=int, required=True, help="column that has symbols")
+@click.option('--outCorrected', type=click.File('w'), required=True, help="file to use for output")
+@click.option('--outUnmatched', type=click.File('w'), required=True, help="file to use for output")
+def cli(mafnames,name_to_entrez, entrez, col_entrez, col_symbol, outCorrected, outUnmatched):
+	MAF_names = dict()
 
-MAF_names = dict()
+	#pull TCGA names from file
+	for line in mafnames:
+		if len(line) > 0:
+			split_line = line.split("\t")
+			symbol = split_line[0].rstrip()
+			MAF_names[symbol.upper()] = symbol
+	mafnames.close()
 
-#pull TCGA names from file
-for line in args.mafnames:
-	if len(line) > 0:
-		split_line = line.split("\t")
-		symbol = split_line[0].rstrip()
-		MAF_names[symbol.upper()] = symbol
-args.mafnames.close()
+	Name2EntrezID = dict()
 
-Name2EntrezID = dict()
+	for line in name_to_entrez:
+		# line = line.rstrip()
+		if len(line) > 0:
+			line_split = line.split("\t")
+			symbol = line_split[0].upper().rstrip()
+			entrez_id = line_split[1].rstrip()
+			if symbol not in Name2EntrezID:
+				if len(symbol) > 0 and len(entrez_id) > 0 and entrez_id != "0":
+					Name2EntrezID[symbol] = entrez_id
+			# else:
+			# 	print("Duplicate entry in symbol->entrez file for symbol: %s" % symbol, file=sys.stderr)
+	name_to_entrez.close()
 
-for line in args.name_to_entrez:
-	# line = line.rstrip()
-	if len(line) > 0:
-		line_split = line.split("\t")
-		symbol = line_split[0].upper().rstrip()
-		entrez_id = line_split[1].rstrip()
-		if symbol not in Name2EntrezID:
-			if len(symbol) > 0 and len(entrez_id) > 0 and entrez_id != "0":
-				Name2EntrezID[symbol] = entrez_id
-		# else:
-		# 	print("Duplicate entry in symbol->entrez file for symbol: %s" % symbol, file=sys.stderr)
-args.name_to_entrez.close()
+	EntrezID2ModernSymbol = dict()
 
-EntrezID2ModernSymbol = dict()
+	for line in entrez:
+		if len(line) > 0:
+			line_split = line.split("\t")
+			symbol = line_split[col_symbol].upper().rstrip()
+			entrez_id = line_split[col_entrez].rstrip()
+			if entrez_id not in EntrezID2ModernSymbol:
+				if len(symbol) > 0 and len(entrez_id) > 0:
+					EntrezID2ModernSymbol[entrez_id] = symbol
+			# else:
+			# 	print("Duplicate entry in entrez->symbol file for entrez_id: %s" % entrez_id, file=sys.stderr)
 
-for line in args.entrez:
-	if len(line) > 0:
-		line_split = line.split("\t")
-		symbol = line_split[args.col_symbol].upper().rstrip()
-		entrez_id = line_split[args.col_entrez].rstrip()
-		if entrez_id not in EntrezID2ModernSymbol:
-			if len(symbol) > 0 and len(entrez_id) > 0:
-				EntrezID2ModernSymbol[entrez_id] = symbol
-		# else:
-		# 	print("Duplicate entry in entrez->symbol file for entrez_id: %s" % entrez_id, file=sys.stderr)
-
-# print("#Old_Symbol\tNew_Symbol", file=args.outCorrected)
-for name in MAF_names:
-	#get entrez_id
-	if name in Name2EntrezID:
-		entrez_id = Name2EntrezID[name]
-		if entrez_id in EntrezID2ModernSymbol:
-			symbol = EntrezID2ModernSymbol[entrez_id]
-			print("%s\t%s" % (MAF_names[name], symbol), file=args.outCorrected)
+	# print("#Old_Symbol\tNew_Symbol", file=args.outCorrected)
+	for name in MAF_names:
+		#get entrez_id
+		if name in Name2EntrezID:
+			entrez_id = Name2EntrezID[name]
+			if entrez_id in EntrezID2ModernSymbol:
+				symbol = EntrezID2ModernSymbol[entrez_id]
+				print("%s\t%s" % (MAF_names[name], symbol), file=outCorrected)
+			else:
+				print("%s\t" % (MAF_names[name]), file=outUnmatched)
 		else:
-			print("%s\t" % (MAF_names[name]), file=args.outUnmatched)
-	else:
-		print("%s\t" % (MAF_names[name]), file=args.outUnmatched)
-	#convert entrez_id back to symbol
-args.outCorrected.close()
-args.outUnmatched.close()
+			print("%s\t" % (MAF_names[name]), file=outUnmatched)
+		#convert entrez_id back to symbol
+	outCorrected.close()
+	outUnmatched.close()
+
+if __name__ == "__main__":
+	cli()
