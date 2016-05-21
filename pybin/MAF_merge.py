9644c2e0e33253552852cdacd85889c7548686a2
Michael Gooch <goochmi@gmail.com>
2016-05-21 16:39:55 -0400

    avoid header printing

--- pybin/MAF_merge.py
+++ pybin/MAF_merge.py
@@ -7,8 +7,10 @@ import GenericFormats.MAF
 @click.command(help="Count # of entries per gene in Util file")
 @click.option("--maf", type=click.File('r'), required=True, multiple=True, help="file containing Util entries")
 @click.option('--out', type=click.File('w'), required=False, default=sys.stdout, help='path to use for output files')
-def main(maf, out):
-	print(GenericFormats.MAF.Entry.get_header_line(), file=out)
+@click.option('--noheader', type=bool(), required=False, default=False, help="disable header line in output")
+def main(maf, out, noheader):
+	if not noheader:
+		print(GenericFormats.MAF.Entry.get_header_line(), file=out)
 	for maf_file in maf:
 		entries = GenericFormats.MAF.File.get_all_entries_from_filehandle(maf_file)
 		maf_file.close()
