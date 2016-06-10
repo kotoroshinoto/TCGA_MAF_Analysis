051514d485cdb1b5c26072d7a0eb1e0263c2c742
kotoroshinoto <goochmi@gmail.com>
2016-06-10 13:54:23 -0400

    changed interface for exon sizer

--- gooch_maf_tools/commands/lengths/exon_sizer.py
+++ gooch_maf_tools/commands/lengths/exon_sizer.py
@@ -46,18 +46,6 @@ def read_bed_and_map_names(bed_handle, name_map):
 	return bed_exon_length_map
 
 
-def read_namefile(name_handle, col_list):
-	from_col = col_list[0]
-	to_col = col_list[1]
-	name_map = dict()
-	#build map of names
-	for name in name_handle:
-		nametuple = next_name(name, from_col, to_col)
-		if nametuple is not None:
-			name_map[nametuple[0]] = nametuple[1]
-	return name_map
-
-
 def merge_and_write_bed_dicts(primary, secondary, outfilehandle):
 
 	if (primary is None) and (secondary is None):
@@ -83,8 +71,20 @@ def merge_and_write_bed_dicts(primary, secondary, outfilehandle):
 	return
 
 
-def read_files_and_map_names(handlelist, col_list):
-	name_map = read_namefile(handlelist[1], col_list)
+def read_namefile(handlelist):
+	from_col = handlelist[2]
+	to_col = handlelist[3]
+	name_map = dict()
+	#build map of names
+	for name in handlelist[1]:
+		nametuple = next_name(name, from_col, to_col)
+		if nametuple is not None:
+			name_map[nametuple[0]] = nametuple[1]
+	return name_map
+
+
+def read_files_and_map_names(handlelist):
+	name_map = read_namefile(handlelist)
 	return read_bed_and_map_names(handlelist[0], name_map)
 
 
@@ -119,23 +119,23 @@ def argcheck(parser):
 	return args
 
 
+# @click.option('--ucsc_col', type=int, nargs=2, help="")
+# @click.option('--refseq_col', type=int, nargs=2, help="")
 @click.command(help="Compute exonic sizes of genes and relate them to HUGO IDs")
-@click.option('--ucsc', nargs=2, type=click.File('r'), help="first path is to a bed file containing genes & exons from UCSC, 2nd path is to path to a file relating BED file names to desired names")
-@click.option('--refseq', nargs=2, type=click.File('r'), help="first path is to a bed file containing genes & exons from REFSEQ, 2nd path is to path to a file relating BED file names to desired names")
-@click.option('--ucsc_col', type=int, nargs=2, help="pair of 0-based index values for parsing the names file, first column's names match those from the BED file, second names match those to use in the output")
-@click.option('--refseq_col', type=int, nargs=2, help="pair of 0-based index values for parsing the names file, first column's names match those from the BED file, second names match those to use in the output")
+@click.option('--ucsc', type=(click.File('r'), click.File('r'), int, int), help="first path is to a bed file containing genes & exons from UCSC, \n2nd path is to path to a file relating BED file names to desired names. \nInts are a pair of 0-based index values for parsing the names file, first column's names match those from the BED file, second names match those to use in the output")
+@click.option('--refseq', type=(click.File('r'), click.File('r'), int, int), help="first path is to a bed file containing genes & exons from REFSEQ, \n2nd path is to path to a file relating BED file names to desired names. \nInts are a pair of 0-based index values for parsing the names file, first column's names match those from the BED file, second names match those to use in the output")
 @click.option('--out', type=click.File('w'), default=sys.stdout, help="output file")
-def cli(ucsc, refseq, ucsc_col, refseq_col, out):
+def cli(ucsc, refseq, out):
 	if len(refseq) == 0 and len(ucsc) == 0:
 		raise click.BadArgumentUsage("Must give at least one of the following: --ucsc --refseq")
-	if len(ucsc) == 2 and len(ucsc_col) == 2:
-		ucsc_bed = read_files_and_map_names(ucsc, ucsc_col)
+	if len(ucsc) == 4:
+		ucsc_bed = read_files_and_map_names(ucsc)
 	else:
-		raise click.BadArgumentUsage("Must provide --ucsc_col when using --ucsc")
-	if len(refseq) == 2 and len(refseq_col) == 2:
-		refseq_bed = read_files_and_map_names(refseq, refseq_col)
+		ucsc_bed = None
+	if len(refseq) == 4:
+		refseq_bed = read_files_and_map_names(refseq)
 	else:
-		raise click.BadArgumentUsage("Must provide --refseq_col when using --refseq")
+		refseq_bed = None
 
 	return merge_and_write_bed_dicts(ucsc_bed, refseq_bed, out)
 
